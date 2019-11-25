1e613807dae401de2b99258406b228a1e1e5dc3e
vikmetodiev <vik.metodiev@abv.bg>
2019-11-25 14:41:06 +0200

    third commit

--- src/routes/Animes/OnePiece/OPStory.js
+++ src/routes/Animes/OnePiece/OPStory.js
@@ -1,11 +1,23 @@
 import React from "react"
-import {Button} from "reactstrap"
-function OPStory(){
-    return(
-        <div>
-            <Button href="/Animes">Back</Button>
-            One Piece Story
-        </div>
+import { Button, Container } from "reactstrap"
+function OPStory() {
+    return (
+        <Container className="backImg3">
+            <h1 style={{ color: "azure" }}>One Piece Story<Button href="/Animes">Back</Button></h1>
+                <h2 className="onePieceStory">One Piece is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized
+                    in Shueisha's Weekly Shōnen Jump magazine since July 22, 1997, and has been collected into 94 tankōbon volumes.
+                    The story follows the adventures of Monkey D. Luffy, a boy whose body gained the properties of rubber after
+                    unintentionally eating a Devil Fruit. With his crew of pirates, named the Straw Hat Pirates, Luffy explores the
+                    Grand Line in search of the world's ultimate treasure known as "One Piece" in order to become the next King of the
+                    Pirates. The manga spawned a media franchise, having been adapted into a festival film produced by Production I.G,
+                    and an anime series produced by Toei Animation, which began broadcasting in Japan in 1999. Additionally,
+                    Toei has developed thirteen animated feature films, one original video animation and thirteen television specials.
+                    Several companies have developed various types of merchandising and media, such as a trading card game and numerous video games.
+                    The manga series was licensed for an English language release in North America and the United Kingdom by Viz Media and in Australia
+                    by Madman Entertainment. The anime series was licensed by 4Kids Entertainment for an English-language release in North America in 2004,
+                before the license was dropped and subsequently acquired by Funimation in 2007.</h2>
+                <div style={{marginBottom:150}}> <br/></div>
+        </Container>
     )
 }
 export default OPStory
